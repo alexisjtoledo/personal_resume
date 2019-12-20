b9e5af284968bb69df2ea47214132c61d7e8137c
Alexis Javier Toledo <alexis.toledo@live.com>
2019-12-20 03:27:49 -0300

    react first version

---
+++ cv-app/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"; // React Router
+
+// Importing Styles
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap 4
+import "./index.css"; // Custom CSS
+
+// Importing Components
+import Sidebar from "./components/Sidebar.js"; // Sidebar
+import Navigation from "./components/Navigation.js"; // Navigation
+import ScrollToTop from "./components/ScrollToTop"; // Scroll To Top Button
+import About from "./components/About.js"; // About
+import Experience from "./components/Experience.js"; // Experience
+import Education from "./components/Education.js"; // Education
+import Skills from "./components/Skills.js"; // Skills
+import Portfolio from "./components/Portfolio.js"; // Portfolio
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <div className="wrapper">
+        <Sidebar />
+        <Redirect from="/" to="/about" />
+        <Route path="/about" component={About} />
+        <Route path="/experience" component={Experience} />
+        <Route path="/education" component={Education} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/portfolio" component={Portfolio} />
+      </div>
+      <ScrollToTop />
+    </Router>
+  );
+}
+
+export default App;
---
+++ cv-app/src/components/Education.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+
+export class Education extends Component {
+    componentDidMount() {
+        window.scrollTo(0,0);
+    }
+    render() {
+        return (
+            <div id="content">
+            <div className="main-article">
+                <h1>My education</h1>
+                <div className="timeline">
+                    <div className="job right-job">
+                        <div className="content">
+                            <h3>2018 - 2021</h3>
+                            <h4>Higher Technician In Programming</h4>
+                            <h5>National Technological University</h5>
+                            <p>Grade Average &#40;GPA&#41;: 8.36</p>
+                        </div>
+                    </div>
+                    <div className="job left-job">
+                        <div className="content">
+                            <h3>2017 - 2018</h3>
+                            <h4>Big Data & Business Analytics Diploma</h4>
+                            <h5>University Siglo 21</h5>
+                            <p>Grade Average &#40;GPA&#41;: 76.87</p>
+                            <p>Modules:
+                                <a className="job-tag" href="https://web.capabilia.org/es/CertificateLicense/QR/NDg5NDNANzBAMTQzOEAxQDE4NjQ3" target="_blank" rel="noopener noreferrer">Data Governance & Data Innovation</a>,
+                                <a className="job-tag" href="https://web.capabilia.org/es/CertificateLicense/QR/NDg5NDNANzBAMTE4M0AxQDE4NjQ3" target="_blank" rel="noopener noreferrer">Marketing Intelligence</a>,
+                                <a className="job-tag" href="https://web.capabilia.org/es/CertificateLicense/QR/NDg5NDNANzBAMTE4MEAxQDE4NjQ3" target="_blank" rel="noopener noreferrer">Advanced Analytics</a>,
+                                <a className="job-tag" href="https://web.capabilia.org/es/CertificateLicense/QR/NDg5NDNANzBAMTE4MkAxQDE4NjQ3" target="_blank" rel="noopener noreferrer">Measuring tools</a>,
+                                <a className="job-tag" href="https://web.capabilia.org/es/CertificateLicense/QR/NDg5NDNANzBAMTE3OEAxQDE4NjQ3" target="_blank" rel="noopener noreferrer">Data Warehouse & BI</a>,
+                                <a className="job-tag" href="https://web.capabilia.org/es/CertificateLicense/QR/NDg5NDNANzBAMTE3OUAxQDE4NjQ3" target="_blank" rel="noopener noreferrer">Dashboarding, Scorecards and Data Visualization</a>,
+                                <a className="job-tag" href="https://web.capabilia.org/es/CertificateLicense/QR/NDg5NDNANzBAMTE4MUAxQDE4NjQ3" target="_blank" rel="noopener noreferrer">Marketing Analytics Strategies</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="job right-job">
+                        <div className="content">
+                            <h3>2011 - 2017</h3>
+                            <h4>Degree in law</h4>
+                            <h5>National University of Córdoba</h5>
+                            <p>Grade Average &#40;GPA&#41;: 7.14</p>
+                        </div>
+                    </div>
+                    <div className="job left-job">
+                        <div className="content">
+                            <h3>2019</h3>
+                            <h4>CEFR levels C1 & C2</h4>
+                            <h5>English for IT</h5>
+                            <p><a href="https://www.english4it.com/profiles/112780" target="_blank" rel="noopener noreferrer">Certificate</a></p>
+                        </div>
+                    </div>
+                    <div className="job right-job">
+                        <div className="content">
+                            <h3>2019</h3>
+                            <h4>Cloud Computing</h4>
+                            <h5>Google Activate</h5>
+                            <p>Credential ID: <strong>9SS 8GR 726</strong></p>
+                            <p><a href="https://learndigital.withgoogle.com/activate/validate-certificate-code" target="_blank" rel="noopener noreferrer">Certificate verification</a></p>
+                        </div>
+                    </div>
+                    <div className="job left-job">
+                        <div className="content">
+                            <h3>2019</h3>
+                            <h4>Introduction to Web Development</h4>
+                            <h5>UA - Universidad de Alicante</h5>
+                            <p>Credentials ID: <strong>ZUC WSC MHK</strong> | <strong>3AL 95T EVX</strong></p>
+                            <p><a href="https://learndigital.withgoogle.com/activate/validate-certificate-code" target="_blank" rel="noopener noreferrer">Certificate verification</a></p>    
+                        </div>
+                    </div>
+                    <div className="job right-job">
+                        <div className="content">
+                            <h3>2018</h3>
+                            <h4>Mobile Apps Development</h4>
+                            <h5>Universidad Complutense de Madrid</h5>
+                            <p>Credential ID: <strong>HLJ WWP QLQ</strong></p>
+                            <p><a href="https://learndigital.withgoogle.com/activate/validate-certificate-code" target="_blank" rel="noopener noreferrer">Certificate verification</a></p>
+                        </div>
+                    </div>
+                    <div className="job left-job">
+                        <div className="content">
+                            <h3>2018</h3>
+                            <h4>Standarized English Test: C1 Advanced</h4>
+                            <h5>EF English First</h5>
+                            <p><a href="https://www.efset.org/cert/n9J81B" target="_blank" rel="noopener noreferrer">Certificate</a></p>
+                        </div>
+                    </div>
+                    <div className="job right-job">
+                        <div className="content">
+                            <h3>2018</h3>
+                            <h4>Digital Marketing Fundamentals</h4>
+                            <h5>IAB Spain</h5>
+                            <p>Credential ID: <strong>LB5 6Z7 ULD</strong></p>
+                            <p><a href="https://learndigital.withgoogle.com/activate/validate-certificate-code" target="_blank" rel="noopener noreferrer">Certificate verification</a></p>
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <a href="https://www.linkedin.com/in/alexistoledo/?locale=en_US" target="_blank" rel="noopener noreferrer" className="no-link">
+                    <button type="button" className="btn btn-primary btn-lg btn-block btn-about"><i className="fa fa-linkedin-square" aria-hidden="true"></i> MORE INFORMATION</button>
+                </a>
+                        
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Education
+
